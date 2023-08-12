399c0ccfe9aac40596c6966c843aea3f62fd65a4
Neil Casas <neilalfonzcasas@gmail.com>
2023-08-12 18:38:57 +0800

    Created playerRound conditions

--- javascript.js
+++ javascript.js
@@ -1,8 +1,47 @@
 
-// Play a round and ompare user choice to computer choice
+// Play a round and compare user choice to computer choice
 function playRound (playerSelection, computerSelection) {
-    // code goes here
+    
+    // If player and computer choice are the same, return tie
+    if (playerSelection === computerSelection) {
+        return 'tie!';
 
+    // If player choice is rock and computer choice is paper
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+
+            // Return 'Computer wins the round'
+            return 'Computer wins the round!';
+        
+         // Otherwise, return 'you win the round'
+        } else {
+            return 'You win the round!';
+        }
+
+    // If player choice is paper and computer choice is scissors
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+
+            // Return 'computer wins the round'
+            return 'Computer wins the round';
+
+        // Otherwise, return 'You win the round'
+        } else {
+            return 'You win the round!';
+        }
+
+    // If player choice is scissors and computer choice is rock
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+
+            // Return 'computer wins the round'
+            return 'Computer wins the round';
+        
+        // Otherwise, return 'You win the round'
+        } else {
+            return 'You win the round';
+        }
+    }
 }
 
 const playerSelection = "rock";
