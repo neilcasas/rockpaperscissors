6dcd457eae4a6163b4ee73f2dcb497b496392860
Neil Casas <neilalfonzcasas@gmail.com>
2023-08-12 18:26:29 +0800

    Created initial structure for playRound function

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+// Play a round and ompare user choice to computer choice
+function playRound (playerSelection, computerSelection) {
+    // code goes here
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
