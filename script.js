8a9b27971bafe2036aa2c987f28cc229f8c1e2e1
Neil Casas <neilalfonzcasas@gmail.com>
2023-09-14 18:49:56 +0800

    Added rock paper scissor buttons

--- script.js
+++ script.js
@@ -1,84 +1,56 @@
+// Initialize buttons
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const play = document.querySelector('.play');
+
 // Initialize userScore and computerScore
 let userScore = 0;
 let computerScore = 0;
 
-// Play until either userScore or computerScore reaches 5
-do {
+// Computer randomly selects a choice
+function getComputerChoice() {
+    const array = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
 
-    // Computer randomly selects a choice
-    function getComputerChoice() {
-        const array = ['rock','paper','scissors'];
-        const random = Math.floor(Math.random() * array.length);
-        return(array[random]);
-    }
-
-    // Play a round and compare user choice to computer choice
-    function playRound (playerSelection, computerSelection) {
-        
-        // If player and computer choice are the same, return tie
-        if (playerSelection === computerSelection) {
-            alert(`Tie! Score - ${userScore} : ${computerScore}`);
-
-        // If player choice is rock and computer choice is paper
-        } else if (playerSelection === 'rock') {
-            if (computerSelection === 'paper') {
-
-                // Add a point to computerScore and return 'Computer wins the round'
-                ++computerScore;
-                alert(`Computer wins the round! Score - ${userScore} : ${computerScore}`);
-
-            // Otherwise, add a point to userScore and return 'You win the round'
-            } else {
-                ++userScore;
-                alert(`You win the round! Score - ${userScore} : ${computerScore}`);
-            }
-
-        // If player choice is paper and computer choice is scissors
-        } else if (playerSelection === 'paper') {
-            if (computerSelection === 'scissors') {
-
-                // Add a point to computerScore and return 'Computer wins the round'
-                ++computerScore;
-                alert(`Computer wins the round! Score - ${userScore} : ${computerScore}`);
-
-            // Otherwise, add a point to userScore and return 'You win the round'
-            } else {
-                ++userScore;
-                alert(`You win the round! Score - ${userScore} : ${computerScore}`);
-            }
-
-        // If player choice is scissors and computer choice is rock
-        } else if (playerSelection === 'scissors') {
-            if (computerSelection === 'rock') {
-
-                // Add a point to computerScore and return 'Computer wins the round'
-                ++computerScore;
-                alert(`Computer wins the round! Score - ${userScore} : ${computerScore}`);
-            
-            // Otherwise, add a point to userScore and return 'You win the round'
-            } else {
-                ++userScore;
-                alert(`You win the round! Score - ${userScore} : ${computerScore}`);
-            }
-        }
-    }
-
-    // Player choice 
-    const playerSelection = prompt('rock, paper or scissors?');
-
-    // Assign computerSelection to the result of getComputerChoice()
+// Play a round and compare user choice to computer choice
+function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
 
-    // Play a round
-    playRound(playerSelection.toLowerCase(),computerSelection);
+    if (playerSelection === computerSelection) {
+        alert(`Tie! Score - ${userScore} : ${computerScore}`);
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        ++userScore;
+        alert(`You win the round! Score - ${userScore} : ${computerScore}`);
+    } else {
+        ++computerScore;
+        alert(`Computer wins the round! Score - ${userScore} : ${computerScore}`);
+    }
 
-} while (userScore < 5 && computerScore < 5);
-
-// Check and prompt winner
-if (userScore > computerScore) {
-     alert('You win the game!');
-} else if (userScore < computerScore) {
-    alert('Computer wins the game!');
-} else {
-    alert('Its a tie!');
+    // Check and prompt winner
+    if (userScore >= 5) {
+        alert('You win the game!');
+        resetGame();
+    } else if (computerScore >= 5) {
+        alert('Computer wins the game!');
+        resetGame();
+    }
 }
+
+// Reset the game
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+}
+
+// Add event listeners to the buttons
+rock.addEventListener('click', () => playerSelection = 'rock');
+paper.addEventListener('click', () => playerSelection = 'paper');
+scissors.addEventListener('click', () => playerSelection = 'scissors');
+play.addEventListener('click', () => playRound(playerSelection));
